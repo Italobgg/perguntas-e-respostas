50f93c1f715b9ea13d29d67d5b36cc0adca74f72
Italo <italobgg00@gmail.com>
2021-09-17 15:34:14 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const connection = require("./database/database");
+const Pergunta = require("./database/Pergurta");
+const { get } = require("express/lib/response");
+const res = require("express/lib/response");
+const Resposta = require("./database/Resposta");
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão ta em dia!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+    
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/",(req, res) => {
+    Pergunta.findAll({raw: true, order:[
+    ['id', 'DESC']    
+    ]}).then(perguntas =>{
+        res.render("index",{
+            perguntas: perguntas
+        });  
+    });
+});
+
+app.get("/perguntar",(req,res)=> {
+    res.render("perguntar");
+})
+
+app.post("/salvarpergunta",(req,res) => {
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    Pergunta.create({
+        titulo: titulo,
+        descricao: descricao
+    }).then(() => {
+        res.redirect("/");
+    });
+});
+
+app.get("/pergunta/:id", (req,res) => {
+    var id = req.params.id;
+    Pergunta.findOne({
+        where: {id: id}
+    }).then(pergunta => {
+        if(pergunta != undefined){
+            Resposta.findAll({
+                where: {perguntaId: pergunta.id},
+                order:[
+                    ['id', 'DESC']
+                ]
+            }).then(respostas => {
+                res.render("pergunta",{
+                pergunta: pergunta,
+                respostas: respostas
+            });
+            });
+        }else{
+            res.redirect("/");
+        }
+    });
+});
+
+app.post("/responder",(req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaId: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
+    });
+});
+
+app.listen(8080,()=>{ console.log("Ta voando meu parceiro");});
---
+++ script.js
@@ -0,0 +1,32 @@
+function enviarScript(scriptText){
+    const lines = scriptText.split("\n"); 
+
+    let i = 0;       
+
+    setInterval(() => { 
+        if(i >= lines.length) return
+
+        if(lines[i].trim() != ''){ 
+            console.log(lines[i]); 
+    
+            window.InputEvent = window.Event || window.InputEvent;
+    
+            const event = new InputEvent('input', { bubbles: true });
+    
+            const textbox = document.querySelector('#main div._13NKt');
+    
+            textbox.textContent = lines[i];
+    
+            textbox.dispatchEvent(event);
+    
+            document.querySelector("#main span[data-testid=send]").click();                   
+
+        }
+        i++;
+    }, 250)
+}
+
+enviarScript(`
+
+
+`);
